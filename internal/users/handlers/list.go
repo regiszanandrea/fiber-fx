8c882f6106d8a585fe3ac35e60351192fe04c7b1
regiszanandrea <regiszanandrea@gmail.com>
2022-01-16 11:24:01 -0300

    feat: empty space

--- internal/users/handlers/list.go
+++ internal/users/handlers/list.go
@@ -34,7 +34,7 @@ func List(c *fiber.Ctx) error {
 	v1 := m["k1"]
 	fmt.Println("v1: ", v1)
 
-	fmt.Println("len:", len(m))
+	fmt.Println("len :", len(m))
 
 	delete(m, "k2")
 	fmt.Println("map:", m)
