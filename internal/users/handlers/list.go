b7298d1cc2f8f7ae7d63aff88a800b7a1054c620
regiszanandrea <regiszanandrea@gmail.com>
2022-01-16 11:13:06 -0300

    feat: add trash code

--- internal/users/handlers/list.go
+++ internal/users/handlers/list.go
@@ -1,10 +1,49 @@
 package handlers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+	"sort"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func List(c *fiber.Ctx) error {
 	return c.JSON(fiber.Map{
 		"name": "john",
 		"age":  20,
 	})
+
+	strs := []string{"c", "a", "b"}
+	sort.Strings(strs)
+	fmt.Println("Strings:", strs)
+
+	ints := []int{7, 2, 4}
+	sort.Ints(ints)
+	fmt.Println("Ints:   ", ints)
+
+	s := sort.IntsAreSorted(ints)
+	fmt.Println("Sorted: ", s)
+
+	m := make(map[string]int)
+
+	m["k1"] = 7
+	m["k2"] = 13
+
+	fmt.Println("map:", m)
+
+	v1 := m["k1"]
+	fmt.Println("v1: ", v1)
+
+	fmt.Println("len:", len(m))
+
+	delete(m, "k2")
+	fmt.Println("map:", m)
+
+	_, prs := m["k2"]
+	fmt.Println("prs:", prs)
+
+	n := map[string]int{"foo": 1, "bar": 2}
+	fmt.Println("map:", n)
+
+	return nil
 }
